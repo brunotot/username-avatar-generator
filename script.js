fc45e80c2e41d4d0e3732a466e7e467660895c62
crorisvanjski4 <crorisvanjski4@crorisvanjski4>
2022-11-16 13:16:58 +0100

    added fullscreen compatibility via search param fullscreen=true

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-var textInput = document.getElementById('textInput');
-var widthInput = document.getElementById('widthInput');
-var colorInput = document.getElementById('colorInput');
-var invertColorChkbx = document.getElementById('invertColorChkbx');
+handleFullScreen();
+var textInput = document.getElementById("textInput");
+var widthInput = document.getElementById("widthInput");
+var colorInput = document.getElementById("colorInput");
+var invertColorChkbx = document.getElementById("invertColorChkbx");
 
-function render(value, width, invertColorFlag) {
-  displayAvatar('displayDiv', {
-    text: value, 
-    width: width,
-    invertColor: invertColorFlag
-  })
-  colorInput.value = document.getElementById('displayDiv').style.backgroundColor
+function handleFullScreen() {
+	let params = new URLSearchParams(document.location.search);
+	let isFullScreen = params.get("fullscreen") === "true";
+	if (isFullScreen) {
+		document.querySelector("html").setAttribute("fullscreen", "");
+	}
 }
 
-textInput.addEventListener('input', e => {
-  var value = e.currentTarget.value;
-  render(value, widthInput.value, invertColorChkbx.checked);
-})
+function render(value, width, invertColorFlag) {
+	displayAvatar("displayDiv", {
+		text: value,
+		width: width,
+		invertColor: invertColorFlag,
+	});
+	colorInput.value =
+		document.getElementById("displayDiv").style.backgroundColor;
+}
 
-widthInput.addEventListener('input', e => {
-  var value = e.currentTarget.value;
-  var valid = CSS.supports('width', value);
-  if (valid) {
-    widthInput.classList.remove('is-invalid')
-  } else {
-    widthInput.classList.add('is-invalid')
-  }
-  var width = value ? value : DEFAULT_AVATAR_DIV_WIDTH;
-  render(textInput.value, width, invertColorChkbx.checked);
-})
+textInput.addEventListener("input", (e) => {
+	var value = e.currentTarget.value;
+	render(value, widthInput.value, invertColorChkbx.checked);
+});
 
-invertColorChkbx.addEventListener('change', function() {
-  var checked = this.checked;
-  render(textInput.value, widthInput.value, checked)
-})
+widthInput.addEventListener("input", (e) => {
+	var value = e.currentTarget.value;
+	var valid = CSS.supports("width", value);
+	if (valid) {
+		widthInput.classList.remove("is-invalid");
+	} else {
+		widthInput.classList.add("is-invalid");
+	}
+	var width = value ? value : DEFAULT_AVATAR_DIV_WIDTH;
+	render(textInput.value, width, invertColorChkbx.checked);
+});
 
-textInput.dispatchEvent(new Event('input'))
+invertColorChkbx.addEventListener("change", function () {
+	var checked = this.checked;
+	render(textInput.value, widthInput.value, checked);
+});
+
+textInput.dispatchEvent(new Event("input"));
